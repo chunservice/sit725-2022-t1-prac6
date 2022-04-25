db0f5749e80b1c269f3b4cea8446590eee0bb4e2
chunservice <anson.it@yahoo.com>
2022-04-25 13:44:34 +1000

    Updates: further-test.js strings & special char

--- test/further-test.js
+++ test/further-test.js
@@ -1,6 +1,7 @@
 var expect = require("chai").expect;
 var request = require("request");
 
+// Test if the inputs are one string and one number
 describe("Add string and number", function() {
     var url = "http://localhost:3000/addTwoNumbers/a/2";
     it("should return status 200", function(done) {
@@ -10,6 +11,31 @@ describe("Add string and number", function() {
         });
     });
     it("returns statusCode key in body to check if api gives right result should be 400", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.statusCode).to.equal(400);
+            done()
+        });
+    });
+    it("returns the result as null", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.result).to.be.a('null');
+            done()
+        });
+    });
+});
+
+// Test if the inputs are one string and one special character
+describe("Add string and special character", function() {
+    var url = "http://localhost:3000/addTwoNumbers/a/*";
+    it("should return status 200", function(done) {
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done()
+        });
+    });
+    it("returns statusCode key in body to check if api gives right result should be 400", function (done) {
         request(url, function(error, response, body) {
             body = JSON.parse(body)
             expect(body.statusCode).to.equal(400);
